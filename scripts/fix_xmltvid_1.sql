44ee2f2a07ca5b324a6d9bf715ebaac981ae4d0a
Zabrimus <github@hannebauer.org>
2025-05-29 21:28:48 +0200

    experimental change

--- scripts/fix_xmltvid_1.sql
+++ scripts/fix_xmltvid_1.sql
@@ -1,6 +1,5 @@
 select x1.id x1, x2.id x2
 from xmltvid x1, xmltvid x2
-where x1.name = x2.name
-  and x1.country = x2.country
-  and x1.xmltv_id <> x2.xmltv_id
-  and x2.xmltv_id like '--%';
+where upper(x1.name) = upper(x2.name)
+  and coalesce(x1.country, '-') = coalesce(x2.country, '-');
+  and x1.xmltv_id <> x2.xmltv_id;
--- scripts/fix_xmltvid_2.sql
+++ scripts/fix_xmltvid_2.sql
@@ -3,6 +3,6 @@ select distinct IIF(x1.id  < x2.id, x1.id, x2.id) x1,
                 IIF(x1.id  < x2.id, x1.xmltv_id, x2.xmltv_id) xmltv_id1,
                 IIF(x1.id  > x2.id, x1.xmltv_id, x2.xmltv_id) xmltv_id2
 from xmltvid x1, xmltvid x2
-where x1.name = x2.name
-  and x1.country = x2.country
+where upper(x1.name) = upper(x2.name)
+  and coalesce(x1.country, '-') = coalesce(x2.country, '-')
   and x1.xmltv_id <> x2.xmltv_id
